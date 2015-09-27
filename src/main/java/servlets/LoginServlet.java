6d87d2a2b2dbf0a34d6ee12dee420a084c34ad02
Ihor Starodubets <ihorstarodubets@gmail.com>
2015-09-27 16:27:26 +0300

    refactoringÐ

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -38,6 +39,8 @@ public class LoginServlet extends HttpServlet {
                 UserDAO userDAO = new UserDAOImpl();
                 User user = userDAO.getUserBy(email, password);
                 if(user.getEmail() != null && user.getPassword() != null ) {
+                    HttpSession session = request.getSession(true);
+                    session.setAttribute("user", user);
                     response.sendRedirect("/home.jsp");
                 }
                 else {
