3135dcf727d439761908348b19648550b903b008
Ihor Starodubets <ihorstarodubets@gmail.com>
2015-11-20 18:07:04 +0200

    modified TestSuite

--- service/src/test/java/TestSuite.java
+++ service/src/test/java/TestSuite.java
@@ -4,6 +4,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         AuthorizationServiceImplTest.class,
+        AuthorizationTest.class,
         CommentServiceImplTest.class,
         NewsServiceImplTest.class,
         RelationServiceImplTest.class,
