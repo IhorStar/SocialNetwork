1377be2b9d29167c8ddd1368e4a4fea9d010d934
Ihor Starodubets <ihorstarodubets@gmail.com>
2015-11-24 15:01:07 +0200

    modified AuthentificationFilter test

--- servlet/src/test/java/AuthentificationFilterTest.java
+++ servlet/src/test/java/AuthentificationFilterTest.java
@@ -5,6 +5,7 @@ import servlets.AuthenticationFilter;
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import static org.mockito.Mockito.*;
 
@@ -12,6 +13,7 @@ public class AuthentificationFilterTest {
     HttpServletRequest request;
     HttpServletResponse response;
     FilterChain filterChain;
+    HttpSession session;
     AuthenticationFilter authenticationFilter;
 
     @Before
@@ -19,15 +21,31 @@ public class AuthentificationFilterTest {
         request = mock(HttpServletRequest.class);
         response = mock(HttpServletResponse.class);
         filterChain = mock(FilterChain.class);
+        session = mock(HttpSession.class);
         authenticationFilter = new AuthenticationFilter();
     }
 
     @Test
-    public void doFilterTest() throws Exception {
+    public void testDoFilter() throws Exception {
         when(request.getSession(false)).thenReturn(null);
-        when(request.getRequestURI()).thenReturn("");
+        when(request.getRequestURI()).thenReturn("login");
         authenticationFilter.doFilter(request, response, filterChain);
-        verify(request).getSession(false);
-        verify(request).getRequestURI();
+        verify(response).sendRedirect("/login.html");
+    }
+
+    @Test
+    public void testDoFilter2() throws Exception {
+        when(request.getSession(false)).thenReturn(null);
+        when(request.getRequestURI()).thenReturn("register");
+        authenticationFilter.doFilter(request, response, filterChain);
+        verify(response).sendRedirect("/login.html");
+    }
+
+    @Test
+    public void testDoFilter3() throws Exception {
+        when(request.getSession(true)).thenReturn(session);
+        when(request.getRequestURI()).thenReturn("html");
+        authenticationFilter.doFilter(request, response, filterChain);
+        verify(filterChain).doFilter(request, response);
     }
 }
